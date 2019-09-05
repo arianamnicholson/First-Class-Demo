c50e08805c0ba77a39c57940cb28157f9218c617
Ari <54719942+arianamnicholson@users.noreply.github.com>
2019-09-05 15:59:40 -0400

    Add files via upload

---
+++ Python Lab Practice.py
@@ -0,0 +1,11 @@
+# Created by Garren Kalter and Ari Nicholson
+# September 5th 2019
+# Lecture 3
+
+pi = 3.14
+r = 2.0
+area = pi * (r**2)
+perim = 2.0*pi*r
+print " Given r: ", r
+print "Area is: ", area
+print " Perimeter is: ", perim

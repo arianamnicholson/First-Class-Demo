bafe8a55cabc9c9a30d408d1de1f64b61bce246f
Ari <54719942+arianamnicholson@users.noreply.github.com>
2019-09-09 16:07:51 -0400

    Create 9-9 class.py

---
+++ 9-9 class.py
@@ -0,0 +1,36 @@
+rapinoe = 15
+print type(rapinoe)
+print float(32)
+print str(42)
+number = 59
+print number / 60.0
+print float(number) / 60
+import math
+print math.pi
+def WorkdayHours () :
+  print ("Mon-Fri: 9-9")
+def WeekendHours () :
+  print ("Weekends: 11-5")
+def ShopHours ():
+  WorkdayHours ()
+  WeekendHours()
+ShopHours()
+def print_twice(bruce):
+  print(bruce)
+  print(bruce)
+
+print_twice("bruce")
+print_twice("Ari")
+def print_four(bruce):
+  print(bruce)
+  print(bruce)
+
+print_four("Ari")
+def square(x):
+  y=x*x
+  return y
+
+print square(7)
+tosquare = 10
+result = square(tosquare)
+print "the result of",tosquare, "squared is", result
